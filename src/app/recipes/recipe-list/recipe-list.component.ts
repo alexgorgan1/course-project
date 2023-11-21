e90fda5b5b8cbe8cac07e7c36cb0e8be6bb33ab2
alexgorgan1 <gorgalex1@gmail.com>
2023-11-21 11:11:55 +0100

    CourseProjekt - 21.11

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,6 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
   recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://assets.bonappetit.com/photos/61b7d09de4a88caa6ceb754b/3:2/w_3000,h_2000,c_limit/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg'),
     new Recipe('A Test Recipe', 'This is simply a test', 'https://assets.bonappetit.com/photos/61b7d09de4a88caa6ceb754b/3:2/w_3000,h_2000,c_limit/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg')
   ];
 
